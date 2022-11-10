7a56d2ac09c72d7a970d59d440fa0e895a24a9d7
mitrajeetdutta <mitrajeet.dutta@embee.co.in>
2022-11-10 11:11:22 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyCardComponent
   ],
   imports: [
     BrowserModule
